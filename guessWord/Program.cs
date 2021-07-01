6b53b600200beadec915e64e739fa179daae3461
ifeolu omo <ifeoluomo@outlook.com>
2021-07-01 13:53:11 +0100

    Add project files.

---
+++ guessWord/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace guessWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SayHi();
+
+            string secretWord = "Right";
+            string guess = "";
+
+            while (guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+
+                if (guess != secretWord)
+                {
+                    Console.WriteLine("You are wrong. \nKindly try again");
+                    Console.ReadLine();
+                }
+
+                // guess = Console.ReadLine();
+
+            }
+
+            Console.Write("You won");
+            Console.ReadLine();
+        }
+
+    }
+}
